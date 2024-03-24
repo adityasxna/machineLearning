82a92dd1a11c16bbb82a5c517436e867bfd62cc0
adityasxna <adityasaxena0022@gmail.com>
2024-03-24 14:50:04 +0530

    third commit

--- headShot.py
+++ headShot.py
@@ -30,8 +30,8 @@ while True:
 
         cv2.circle(img, (fx, fy - 80), 80, (0, 0, 255), 2)
         cv2.putText(img, str(pos), (fx+15, fy-15), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2 )
-        cv2.line(img, (0, fy - 80), (1280, fy), (0, 0, 0), 2)  # x line
-        cv2.line(img, (fx, 720), (fx, 0), (0, 0, 0), 2)  # y line
+        cv2.line(img, (0, fy), (1280, fy), (0, 0, 0), 2)
+        cv2.line(img, (fx, 720), (fx, 0), (0, 0, 0), 2)
         cv2.circle(img, (fx, fy - 80), 15, (0, 0, 255), cv2.FILLED)
         cv2.putText(img, "TARGET LOCKED", (850, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3 )
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,22 +1,7 @@
-import cv2
-import mediapipe as mp
+import numpy as np
+import matplotlib.pyplot as plt 
 
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mphands=mp.solutions.hands
-
-cap = cv2.VideoCapture(0)
-
-hands = mphands.Hands()
-while True:
-    data, image = cap.read()
-    image = cv2.cvtColor(cv2.flip(image,1), cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            mp_drawing.draw_landmarks(
-                image,
-                hand_landmarks, mphands.HAND_CONNECTIONS)
-    
-    cv2.imshow('Handtracker', image)
-    cv2.waitKey(1)
+im = np.arange(10)
+im = im[np.newaxis, :]
+im = np.repeat(im, 10, axis=0)
+plt.imshow(im, cmap='gray')
